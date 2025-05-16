3680a60a12514bf1de6811c8a7f26af077ca1604
ciprian simionov <simionov.ciprian@gmail.com>
2025-05-16 17:04:43 +0300

    updated emag.py

--- emag.py
+++ emag.py
@@ -1,8 +1,6 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
 
@@ -27,7 +25,18 @@ class TestEmag(unittest.TestCase):
         self.chrome.implicitly_wait(3)
         container = self.chrome.find_element(By.CLASS_NAME, "cart-line")
         self.assertTrue(container, "Container is not empty")
-        # pret = self.chrome.find_element(By.XPATH, "//div[@class=\"order-summary-total\"] / div / p[contains(text(), \"7.699\")]")
-        # assert pret, "7.699"
+
+    def test_search_products_with_keys_library(self):
+        self.chrome.find_element(By.CSS_SELECTOR, "input[id=\"searchboxTrigger\"]").send_keys("Iphone 15 pro maxx")
+        self.chrome.find_element(By.CSS_SELECTOR, "input[id=\"searchboxTrigger\"]").send_keys(Keys.BACKSPACE)
+        self.chrome.find_element(By.CSS_SELECTOR, "input[id=\"searchboxTrigger\"]").send_keys(Keys.ENTER)
+        self.chrome.find_element(By.CSS_SELECTOR, "div[class*=\"card-item\"]:nth-of-type(4)").click()
+        self.chrome.find_element(By.CSS_SELECTOR, "a[title=\"512 GB\"]").click()
+        self.chrome.find_element(By.CSS_SELECTOR, "div[class=\"product-buy-area-wrapper\"] > div > button[type=\"submit\"]").click()
+        self.chrome.implicitly_wait(3)
+        self.chrome.find_element(By.XPATH, "//a[contains(text(), \"Vezi detalii cos\")]").click()
+        self.chrome.implicitly_wait(3)
+        pret = self.chrome.find_element(By.XPATH, "//div[@class=\"order-summary-total\"] / div / p[contains(text(), \"8.500\")]")
+        assert pret, "8.500"
 
 
